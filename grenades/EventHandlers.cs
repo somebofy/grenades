1e1ff0347437ea14741ba2a0363f2923fabc42e5
somebofy <gafner-a@bk.ru>
2021-04-14 13:10:11 +0700

    alpha version
    my first plugin.

---
+++ grenades/EventHandlers.cs
@@ -0,0 +1,10 @@
+﻿using Qurre;
+
+namespace grenades
+{
+    public class EventHandlers
+    {
+        private readonly Plugin plugin;
+        public EventHandlers(Plugin plugin) => this.plugin = plugin;
+    }
+}
---
+++ grenades/grenades.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace grenades
+{
+    public class grenades : Qurre.Plugin
+    {
+        public EventHandlers EventHandlers;
+        #region override
+        public override string Developer { get; } = "nekto";
+        public override string Name { get; } = "grenades";
+        public override System.Version Version { get; } = new System.Version(0, 0, 1);
+        public override System.Version NeededQurreVersion { get; } = new System.Version(1, 3, 0);
+        public override void Reload() => base.Reload();
+        public override void Enable() => RegisterEvents();
+        public override void Disable() => UnregisterEvents();
+        #endregion
+
+
+        #region RegEvents
+        private void RegisterEvents()
+        {
+
+        }
+        #endregion
+
+        #region UnregEvents
+        private void UnregisterEvents()
+        {
+
+        }
+        #endregion
+    }
+}
